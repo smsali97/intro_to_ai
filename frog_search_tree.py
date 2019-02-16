73da133c58f9d3805213c9b5ca7fd584cea7eb60
Sualeh Ali <smsali97@gmail.com>
2019-02-16 23:41:28 +0500

    Added Graph with visualisation using NetworkX

--- frog_search_tree.py
+++ frog_search_tree.py
@@ -5,6 +5,7 @@ Created on Mon Feb  4 19:23:53 2019
 @author: sualeh
 
 This program prints complete state space graph and the search tree of the ‘Frog Problem’.
+Goal Node: 2220111
 Also, counts the number of states while generating them
 
 Encoding:
@@ -13,11 +14,13 @@ Each state of the game will be represented in the form of a 7-char string
 2 represents brown frog
 
 Root Node: 1110222
-Goal Node: 2220111
 
 """
 
 import queue
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 class Node():
     def __init__(self,state):
@@ -40,7 +43,11 @@ class FrogGameSearchTree():
     FINAL_STATE = '2220111'
 
 
+    def __str__(self):
+        return str(self.root)
+
     def __init__(self):
+        self.G = nx.DiGraph()
         self.counter = 0
         self.root = Node(self.INITIAL_STATE)
 
@@ -53,8 +60,13 @@ class FrogGameSearchTree():
         return frog is '0'
 
     def getStates(self):
+
         unexplored_nodes = queue.Queue()
         unexplored_nodes.put(self.root)
+
+        # Add starting Node
+        self.G.add_node(self.root.state)
+
         ctr = 0
         while True:
             latest_node = unexplored_nodes.get()
@@ -63,8 +75,9 @@ class FrogGameSearchTree():
 
             else:
                 frogs = latest_node.state
+
+
                 print("Iteration # " + str(ctr))
-                print(str(self.root))
                 ctr += 1
                 for i in range(len(frogs)):
 
@@ -74,6 +87,7 @@ class FrogGameSearchTree():
                         if self.hasNoFrog(frogs[i+1]):
                             # .. 1 0 ... --> .. 0 1 ...
                             new_state = list(frogs)
+
                             new_state[i] = '0'
                             new_state[i+1] = '1'
                             new_state = ''.join(new_state)
@@ -82,6 +96,10 @@ class FrogGameSearchTree():
                             new_node = Node(new_state)
                             latest_node.children.append(new_node)
                             unexplored_nodes.put(new_node)
+
+                            self.add_edge(frogs,new_state)
+
+
 
                         # Jump over the next one
                         if i < len(frogs) - 2 and not self.hasNoFrog(frogs[i+1]) and self.hasNoFrog(frogs[i+2]):
@@ -94,14 +112,27 @@ class FrogGameSearchTree():
                             latest_node.children.append(new_node)
                             unexplored_nodes.put(new_node)
 
+                            self.add_edge(frogs,new_state)
+
+
                     elif self.isBrownFrog(frogs[i]) and i > 0:
                         # Jump to next one
+
                         if self.hasNoFrog(frogs[i-1]):
                             # .. 0 2 .. --> ... 2 0 ..
                             new_state = list(frogs)
                             new_state[i] = '0'
                             new_state[i-1] = '2'
                             new_state = ''.join(new_state)
+
+
+                            # add children
+                            new_node = Node(new_state)
+                            latest_node.children.append(new_node)
+                            unexplored_nodes.put(new_node)
+
+                            self.add_edge(frogs,new_state)
+
 
                         # Jump over the next one
                         if i > 1 and not self.hasNoFrog(frogs[i-1]) and self.hasNoFrog(frogs[i-2]):
@@ -115,9 +146,31 @@ class FrogGameSearchTree():
                             latest_node.children.append(new_node)
                             unexplored_nodes.put(new_node)
 
+                            self.add_edge(frogs,new_state)
+
+
+            if unexplored_nodes.empty():
+                print("Bye\n")
+                return
+
+    def add_edge(self,old_state,new_state):
+        print(old_state + ',' + new_state)
+        # if not self.G.has_node(new_state):
+        #     self.G.add_node(new_state)
+
+        if not self.G.has_edge(old_state,new_state):
+            self.G.add_edge(old_state,new_state)
+
+
+
 def main():
     tree = FrogGameSearchTree()
     tree.getStates()
+    print(tree)
+
+    nx.draw_spectral(tree.G,with_labels = True,node_size=150,font_size=10)
+    plt.savefig("simple_path.png") # save as png
+    plt.show() # display
 
 if __name__ == '__main__':
     main()
